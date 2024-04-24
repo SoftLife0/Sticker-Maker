43724cdb8347056a8b4445b591436cfdb3e7863f
SoftLife0 <onikosiadewale18@gmail.com>
2024-04-24 02:00:40 +0000

    using React Native Image picker library

--- App.js
+++ App.js
@@ -2,18 +2,36 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, View } from 'react-native';
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 const placeholderImage = require('./assets/images/background-image.png')
 
 const App = () => {
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={placeholderImage} />
+        <ImageViewer placeholderImageSource={placeholderImage} selectedImage={selectedImage}/>
       </View>
       
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a Photo" />
+        <Button theme="primary" label="Choose a Photo" onPress={pickImageAsync}/>
         <Button label="Use this Photo" />
       </View>
       <StatusBar style="auto" />
--- components/Button.js
+++ components/Button.js
@@ -2,14 +2,14 @@ import { StyleSheet, Text, View, Pressable } from 'react-native'
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 
-const Button = ({label, theme}) => {
+const Button = ({label, theme, onPress}) => {
 
     if (theme === "primary") {
         return (
           <View style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}>
             <Pressable
               style={[styles.button, { backgroundColor: "#fff" }]}
-              onPress={() => alert('You pressed a button.')}
+              onPress={onPress}
             >
               <FontAwesome
                 name="picture-o"
