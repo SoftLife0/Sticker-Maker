0e72bde175ec5b42ccf14b4314b8e8055fd10b15
SoftLife0 <onikosiadewale18@gmail.com>
2024-04-24 00:47:20 +0000

    let learn React Native

--- App.js
+++ App.js
@@ -13,8 +13,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
+    color: '#fff',
   },
+
 });
